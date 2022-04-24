f53786fedd59670a17913804103107a6b0bfdc74
vilmrunas <vilmrunas@gmail.com>
2022-04-24 14:34:38 +0300

    Initial commit

---
+++ src/main/java/lt/runas/worldclock/WorldClockApplication.java
@@ -0,0 +1,13 @@
+package lt.runas.worldclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldClockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/runas/worldclock/WorldclockApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.runas.worldclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldclockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
